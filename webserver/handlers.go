50c5e612f3e1db65e19968d274e429aa43e48429
Jonathan Hurter <jonathan@hurter.fr>
2016-03-01 19:44:10 +0100

    Templating and linking + Add css style to pages (Sort of) + Submission form + Bootstrap + Linking controllers and workers + custom css - test css

--- webserver/handlers.go
+++ webserver/handlers.go
@@ -17,9 +17,9 @@ func indexHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 // This function is responsible for the Search page response
 // GET : /search?query=<QUERY>
 func searchHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	//query := r.URL.Query().Get("query")
-	//language := r.URL.Query().Get("language")
-	repositories := gitprocessor.GetGithubRepositories("user:docker")
-	stats := filter.Filter(repositories, "Go")
+	query := r.URL.Query().Get("query")
+	language := r.URL.Query().Get("language")
+	repositories := gitprocessor.GetGithubRepositories(query)
+	stats := filter.Filter(repositories, language)
 	render(w, "search.html.tmpl", stats)
 }
