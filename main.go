b662afea9d4ad62febd9aedbe3c542b136d5949a
Jonathan Hurter <jonathan@hurter.fr>
2016-03-01 16:36:26 +0100

    Fetching git repositories list + gitprocessor package + base gitAPI struct + base sling methods + GetGithubRepositories by query

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"log"
 
+	"github.com/johnsudaar/gitngo/gitprocessor"
 	"github.com/johnsudaar/gitngo/webserver"
 )
 
 func main() {
+	log.Println(len(gitprocessor.GetGithubRepositories("docker")))
+
 	log.Fatal(webserver.Start(8080))
 }
