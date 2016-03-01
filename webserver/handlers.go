424c17ea6b519a251b881e33796e2a96c4947c1e
Jonathan Hurter <jonathan@hurter.fr>
2016-03-01 21:22:34 +0100

    Adding logs + log on server start + logging midlleware.

--- webserver/handlers.go
+++ webserver/handlers.go
@@ -5,18 +5,17 @@ import (
 
 	"github.com/johnsudaar/gitngo/filter"
 	"github.com/johnsudaar/gitngo/gitprocessor"
-	"github.com/julienschmidt/httprouter"
 )
 
 // This function is reponsible for the Index response
 // GET : /
-func indexHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+func indexHandler(w http.ResponseWriter, r *http.Request) {
 	render(w, "index.html.tmpl", nil)
 }
 
 // This function is responsible for the Search page response
 // GET : /search?query=<QUERY>
-func searchHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+func searchHandler(w http.ResponseWriter, r *http.Request) {
 	query := r.URL.Query().Get("query")
 	customQuery := r.URL.Query().Get("custom")
 	language := r.URL.Query().Get("language")
--- webserver/webserver.go
+++ webserver/webserver.go
@@ -1,25 +1,44 @@
 package webserver
 
 import (
+	"log"
 	"net/http"
 	"strconv"
+	"time"
 
+	"github.com/gorilla/context"
 	"github.com/julienschmidt/httprouter"
+	"github.com/justinas/alice"
 )
 
 // Start is the function used to launch the webserver
 // The port argument specifies the port number which will be used by the webserver
 // This function will return the error returned by the http.ListenAndServe method
 func Start(port int) error {
-
+	log.Println("[WEB] Loading ...")
 	// Router configuration
 	router := httprouter.New()
-	router.GET("/", indexHandler)
-	router.GET("/search", searchHandler)
+	router.GET(logged("/", indexHandler))
+	router.GET(logged("/search", searchHandler))
 
 	// Assets configuration
 	fs := http.FileServer(http.Dir("ressources/assets/"))
 	router.NotFound = http.StripPrefix("/assets/", fs)
-
+	log.Println("[WEB] Listenning at : :" + strconv.Itoa(port))
 	return http.ListenAndServe(":"+strconv.Itoa(port), router)
+}
+
+// Make httprouter compatible with standard http and make it compatible with alice.
+func logged(p string, h func(http.ResponseWriter, *http.Request)) (string, httprouter.Handle) {
+	return p, loggerMiddleware(alice.New(context.ClearHandler).ThenFunc(h))
+}
+
+// Middleware for logging request statistics.
+func loggerMiddleware(h http.Handler) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		context.Set(r, "params", ps)
+		start := time.Now()
+		h.ServeHTTP(w, r)
+		log.Printf("[WEB] %s %s in %v", r.Method, r.URL.Path, time.Since(start))
+	}
 }
