7dabe55105bd8970647005561ab656e76b1e1318
Jonathan Hurter <jonathan@hurter.fr>
2016-03-01 19:11:27 +0100

    Assets file server + Assets file configuration + Test css file

--- webserver/webserver.go
+++ webserver/webserver.go
@@ -11,8 +11,15 @@ import (
 // The port argument specifies the port number which will be used by the webserver
 // This function will return the error returned by the http.ListenAndServe method
 func Start(port int) error {
+
+	// Router configuration
 	router := httprouter.New()
 	router.GET("/", indexHandler)
 	router.GET("/search", searchHandler)
+
+	// Assets configuration
+	fs := http.FileServer(http.Dir("ressources/assets/"))
+	router.NotFound = http.StripPrefix("/assets/", fs)
+
 	return http.ListenAndServe(":"+strconv.Itoa(port), router)
 }
